ef6c636aea7aa59c6eba55cac247f1e9c60ebec2
Melker Ståhlberg <melker.stahlberg@gmail.com>
2025-08-03 14:13:03 +0200

    Some TC codes removed that were in the wrong file

--- ground_station/receiver/packet_types.py
+++ ground_station/receiver/packet_types.py
@@ -2,7 +2,3 @@
 PACKET_TYPE_HK   = 0b00  # Housekeeping
 PACKET_TYPE_BF   = 0b01  # Bit Flip
 PACKET_TYPE_ACK  = 0b11  # Acknowledgment
-
-#telecommand (3-bit)
-TC_RESET    = 0b000
-TC_RESTART  = 0b001
