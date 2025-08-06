1d3f88b4604d8e1afa8972a7456c5488ef41d248
Melker Ståhlberg <69123065+melkerstahlberg@users.noreply.github.com>
2025-08-06 21:16:08 +0200

    Added file for configuring packet structures more easily, WIP

---
+++ ground_station/receiver/packet_structures.py
@@ -0,0 +1,34 @@
+
+PACKET_HK = {
+    "ID": 2,
+    "SEQ": 16,
+    "RTC": 17,
+    "TEMP": 96,
+    "GNSS": 55,
+    "ALT": 48,
+    "CRC": 16
+}
+
+PACKET_BF = {
+    "ID": 2,
+    "SEQ": 16,
+    "RTC": 17,
+    "BF": 172,
+    "CRC": 16
+}
+
+PACKET_RAD = {
+    "ID": 2,
+    "SEQ": 16,
+    "RTC": 17,
+    "DATA" = 9984,
+    "CRC" = 16
+}
+
+PACKET_ACK = {
+    "ID": 2,
+    "SEQ": 16,
+    "RTC": 17,
+    "ACK": 3,
+    "CRC": 16
+}
