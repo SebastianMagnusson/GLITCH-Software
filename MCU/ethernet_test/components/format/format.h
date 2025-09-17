762e39379f567e614b068220b9bd2fc541111bcc
Melker Ståhlberg <melker.stahlberg@gmail.com>
2025-09-17 15:19:31 +0200

    Header fix

--- MCU/ethernet_test/components/format/format.h
+++ MCU/ethernet_test/components/format/format.h
@@ -11,3 +11,10 @@ uint8_t* unpack_tc(uint8_t* packet);
 /// @param uint8_t* The telecommand data buffer to be formatted.
 /// @return uint8_t*: The formatted telemetry data as a byte array.
 uint8_t* format_tc(uint8_t* data);
+
+/// @brief Takes a telemetry data buffer and formats it into the correct telemetry structure.
+/// @param uint8_t* The telemetry data buffer to be formatted.
+/// @param int The total size of the packet to be created, in bytes.
+/// @param int The number of bits in the data buffer that are actual data (not padding).
+/// @return uint8_t*: The formatted telemetry data as a byte array.
+uint8_t* pack_tm(uint8_t* data, int packet_size, int data_bits)
